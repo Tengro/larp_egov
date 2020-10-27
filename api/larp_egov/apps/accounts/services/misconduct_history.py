37e67aae104415e642bee5f7a4f27dc64c6cd6f7
Andrew Luashchuk <tengro@gmail.com>
2020-10-28 00:50:59 +0200

    Report bot command fixed

--- api/larp_egov/apps/accounts/services/misconduct_history.py
+++ api/larp_egov/apps/accounts/services/misconduct_history.py
@@ -43,10 +43,10 @@ def get_user_misconduct_reports(update):
     user = get_user_by_character_id(code)
     if not user:
         return "No such user exists"
-    requester.send_message('ohfuck')
     reports = MisconductReport.objects.filter(reported_person=user)
     if not reports:
         return "This person has clean record"
+    requester.send_message('ohfuck')
     return '\n\n'.join([x.police_record_string for x in reports])
 
 

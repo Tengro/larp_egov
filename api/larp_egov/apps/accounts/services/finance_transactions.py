456033a234ad89587b23b84e196ae6c14da3d1ca
Andrew Luashchuk <tengro@gmail.com>
2020-10-27 21:50:56 +0200

    Object IDs added

--- api/larp_egov/apps/accounts/services/finance_transactions.py
+++ api/larp_egov/apps/accounts/services/finance_transactions.py
@@ -62,7 +62,6 @@ def create_transaction(update, is_anonymous=False):
         return "Seems like you aren't registered"
     message = update.message.text[6:]
     user_code, amount = message.split(' ')
-    requester.send_message(f"{update.message.text}, {message}, {user_code}, {amount}")
     user = get_user_by_character_id(user_code)
     if not user:
         return "Can\'t find user in database; report not filed"
@@ -70,6 +69,7 @@ def create_transaction(update, is_anonymous=False):
         amount = decimal.Decimal(penalty_amount)
     except decimal.InvalidOperation:
         return "Incorrect amount!"
+    requester.send_message(f"{requester}, {user}, {amount}")
     BankTransaction.create_transaction(requester, user, amount, is_anonymous)
 
 

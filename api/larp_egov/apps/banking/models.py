f0f10c041cf09d3673633f499742351cc7415794
Andrew Luashchuk <tengro@gmail.com>
2020-10-28 00:03:07 +0200

    Transaction bot command fixed

--- api/larp_egov/apps/banking/models.py
+++ api/larp_egov/apps/banking/models.py
@@ -74,6 +74,7 @@ class BankTransaction(CoreModel):
     @classmethod
     def create_transaction(cls, sender, reciever, amount, is_anonymous=False, comment=''):
         if sender.bank_account < amount:
+            sender.send_message('Bank acccount insufficient!')
             raise ValueError('Bank account insufficient')
         transaction = cls.objects.create(
             sender=sender,

5c26cee3d0b0012035341f84c8f8d5103fd779ba
Andrew Luashchuk <tengro@gmail.com>
2020-10-28 00:06:19 +0200

    Transaction bot command fixed

--- api/larp_egov/apps/accounts/services/finance_transactions.py
+++ api/larp_egov/apps/accounts/services/finance_transactions.py
@@ -72,7 +72,7 @@ def create_transaction(update, is_anonymous=False):
     try:
         BankTransaction.create_transaction(requester, user, amount, is_anonymous)
     except ValueError as e:
-        requester.send_message(f'Blyad! {e.message}')
+        requester.send_message(f'Blyad! {e}')
         return e.message
 
 

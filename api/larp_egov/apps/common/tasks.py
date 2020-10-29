b8e377415574ec8df69fd3a46b1c328edb427df2
Andrew Luashchuk <tengro@gmail.com>
2020-10-29 14:14:43 +0200

    Corporation-related commands finished

--- api/larp_egov/apps/common/tasks.py
+++ api/larp_egov/apps/common/tasks.py
@@ -1,9 +1,11 @@
 from larp_egov import celery_app
 import requests
 from django_telegrambot.apps import DjangoTelegramBot
+from larp_egov.apps.accounts.models import UserAccount
 
 
 @celery_app.task
 def refresh_hook():
     token = DjangoTelegramBot.dispatcher.bot.token
     requests.get(f"https://api.telegram.org/bot{token}/setWebhook?url=https://atomlarp.com/bot/{token}/")
+    UserAccount.objects.get_service_account().send_message('Hook refreshed')

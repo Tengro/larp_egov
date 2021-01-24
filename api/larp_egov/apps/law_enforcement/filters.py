f40c851342e4a41810f64cdab278f8ee07357d5a
Andrew Luashchuk <tengro@gmail.com>
2021-01-24 18:44:28 +0200

    Police Dashboard

--- api/larp_egov/apps/law_enforcement/filters.py
+++ api/larp_egov/apps/law_enforcement/filters.py
@@ -1,8 +1,10 @@
 import django_filters
 from larp_egov.apps.law_enforcement.models import MisconductReport
+from larp_egov.apps.accounts.models import UserAccount
 
 
 class MisconductReportFilter(django_filters.FilterSet):
+    officer_in_charge = django_filters.ModelChoiceFilter(queryset=UserAccount.objects.get_police_officers())
 
     class Meta:
         model = MisconductReport

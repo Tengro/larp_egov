11865fe715541a4bf33d4610ba2fba07fa180d55
Andrew Luashchuk <tengro@gmail.com>
2021-01-24 12:35:29 +0200

    Fixes of messaging

---
+++ api/larp_egov/apps/common/bot_commands/safe_message_send.py
@@ -0,0 +1,15 @@
+def split_string(text, chars_per_string):
+    """
+    Splits one string into multiple strings, with a maximum amount of `chars_per_string` characters per string.
+    This is very useful for splitting one giant message into multiples.
+    :param text: The text to split
+    :param chars_per_string: The number of characters per line the text is split into.
+    :return: The splitted text as a list of strings.
+    """
+    return [text[i:i + chars_per_string] for i in range(0, len(text), chars_per_string)]
+
+
+def safe_message_send(bot, chat_id, text):
+    splitted_text = util.split_string(message, 4000)
+    for text in splitted_text:
+        bot.sendMessage(chat_id, text)
